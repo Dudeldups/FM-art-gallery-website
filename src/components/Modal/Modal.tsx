997b43c0fe7ee4007283cf2b6dab28cf413ae841
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2023-11-16 11:46:40 +0100

    added modal location styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,10 @@
 import Button from "../Button/Button";
 
+import mobileSmallMapImage from "../../assets/images/mobile/image-map.png";
+import mobileBigMapImage from "../../assets/images/mobile/image-map@2x.png";
+import tabletMapImage from "../../assets/images/tablet/image-map@2x.png";
+import desktopMapImage from "../../assets/images/desktop/image-map@2x.png";
+
 import "./Modal.scss";
 
 interface ModalProps {
@@ -7,5 +12,46 @@ interface ModalProps {
 }
 
 export default function Modal({ setIsModalShown }: ModalProps) {
-  return <Button setIsModalShown={setIsModalShown} isPrimary={false} />;
+  return (
+    <section className="modal">
+      <picture>
+        <source srcSet={desktopMapImage} media="(min-width: 61.25rem)" />
+        <source srcSet={tabletMapImage} media="(min-width: 43.75rem)" />
+        <source srcSet={mobileBigMapImage} media="(min-width: 23.5rem)" />
+
+        <img
+          className="modal__map-image"
+          src={mobileSmallMapImage}
+          alt="A map for the location of the gallery at 99 King Street, Newport, USA."
+        />
+      </picture>
+
+      <Button setIsModalShown={setIsModalShown} isPrimary={false} />
+
+      <div className="location-bg">
+        <div className="location">
+          <h2 className="location__title">Our Location</h2>
+
+          <div className="location__text">
+            <address>
+              <span className="location__street">99 King Street</span>
+              <span>
+                Newport
+                <br />
+                RI 02840
+                <br />
+                United States of America
+              </span>
+            </address>
+
+            <p className="location__desc">
+              Our newly opened gallery is located near the Edward King House on
+              99 King Street, the Modern Art Gallery is free to all visitors and
+              open seven days a week from 8am to 9pm.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
