cec4197745dab86891b72e47d15125ea63d354fb
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2023-11-17 13:50:58 +0100

    replaced map image with leaflet map

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,7 @@
 import Button from "../Button/Button";
-
-import mobileSmallMapImage from "../../assets/images/mobile/image-map.png";
-import mobileBigMapImage from "../../assets/images/mobile/image-map@2x.png";
-import tabletMapImage from "../../assets/images/tablet/image-map@2x.png";
-import desktopMapImage from "../../assets/images/desktop/image-map@2x.png";
+import Map from "../Map/Map";
 
 import "./Modal.scss";
-import Map from "../Map/Map";
 
 interface ModalProps {
   setIsModalShown: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,23 +11,12 @@ export default function Modal({ setIsModalShown }: ModalProps) {
   return (
     <section className="modal">
       <div className="map-container">
-        <picture className="modal__map-image">
-          <source srcSet={desktopMapImage} media="(min-width: 61.25rem)" />
-          <source srcSet={tabletMapImage} media="(min-width: 43.75rem)" />
-          <source srcSet={mobileBigMapImage} media="(min-width: 23.5rem)" />
-
-          <img
-            src={mobileSmallMapImage}
-            alt="A map for the location of the gallery at 99 King Street, Newport, USA."
-          />
-        </picture>
+        <Map />
 
         <div className="button-container">
           <Button setIsModalShown={setIsModalShown} isPrimary={false} />
         </div>
       </div>
-
-      <Map />
 
       <div className="location-bg">
         <div className="location">
