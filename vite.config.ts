370c4e24e551875aece400530de5eb1fe9b820af
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-02-04 21:24:31 +0100

    added production variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProd = process.env.VITE_PROD === "true";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.PROD ? "/projects/art-gallery" : "/",
+  base: isProd ? "/projects/art-gallery" : "/",
   plugins: [react()],
 });
