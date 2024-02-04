36fb993a3f68aadf916f6557b4bba30e80ccb437
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-02-04 19:46:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //@ts-expect-error Vite but with .env import
   base: import.meta.env.PROD ? "/projects/art-gallery" : "/",
   plugins: [react()],
 });
